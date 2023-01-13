2f8e836a835906bd9d7f0a1b77d1ed636bcb566c
Chris Crane <chrisjcrane@gmail.com>
2023-01-13 09:10:03 -0700

    Add iteration 1 tests for Employee class

---
+++ spec/employee_spec.rb
@@ -0,0 +1,26 @@
+require './lib/employee'
+
+RSpec.describe Employee do
+  before do
+    @bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@bobbi).to be_a(Employee)
+    end
+    
+    xit 'has attributes' do
+      expect(@bobbi.name).to eq("Bobbi Jaeger")
+      expect(@bobbi.age).to eq(30)
+      expect(@bobbi.salary).to eq(100000)
+    end
+  end
+  
+  xdescribe '#give_raise' do
+    it 'increases employee salary by argument amount' do
+      bobbi.give_raise(5000)
+      expect(@bobbi.salary).to eq(105000)
+    end
+  end
+end
