e9e7e73bd0315c103ffdb5d20202f3a9984abf88
Chris Crane <chrisjcrane@gmail.com>
2023-01-13 10:05:15 -0700

    Add test for #add_department

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -9,10 +9,15 @@ RSpec.describe Budget do
     @customer_service = Department.new("Customer Service") 
     @sales = Department.new("Sales") 
 
-    @bobbi = Department.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+    @bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
     @aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})
-    @craig = Department.new({name: "Craig Carle", age: "30", salary: "$200000"})
+    @craig = Employee.new({name: "Craig Carle", age: "30", salary: "$200000"})
     @aaron = Employee.new({name: "Tony Stark", age: "25", salary: "99999999"})
+
+    @customer_service.hire(@bobbi)
+    @customer_service.hire(@aaron)
+    @sales.hire(@craig)
+    @sales.hire(@aaron)
   end
 
   describe '#initialize' do
@@ -23,6 +28,15 @@ RSpec.describe Budget do
     it 'has attributes' do
       expect(@budget.year).to eq(2022)
       expect(@budget.departments).to eq([])
+    end
+  end
+
+  describe '#add_department' do
+    it 'adds a department to the budget' do
+      @budget.add_department(@customer_service)
+      expect(@budget.departments).to eq([@customer_service])
+      @budget.add_department(@sales)
+      expect(@budget.departments).to eq([@customer_service, @sales])
     end
   end
 end
