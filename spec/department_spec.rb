28f4df7d0a571cbfa89df41775468b5e698fa3ba
Chris Crane <chrisjcrane@gmail.com>
2023-01-13 09:17:46 -0700

    Add tests for department #initialize

---
+++ spec/department_spec.rb
@@ -0,0 +1,21 @@
+require './lib/department'
+require './lib/employee'
+
+RSpec.describe Department do
+  before do
+    @customer_service = Department.new("Customer Service") 
+    @bobbi = Department.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+    @aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@customer_service).to be_a(Department)
+    end
+    
+    it 'has attributes' do
+      expect(@customer_service.name.name).to eq("Customer Service")
+      expect(@customer_service.employees).to eq([])
+    end
+  end
+end
